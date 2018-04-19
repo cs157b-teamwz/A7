0fecde8583e951b0f693dfc4505445443abb9a2f
quocnguyen <nguyenq2006@gmail.com>
2018-04-18 21:20:55 -0700

    added page to search course, and fixed bugs for add course page

--- controllers/courses.js
+++ controllers/courses.js
@@ -19,12 +19,26 @@ module.exports.add_course = function(req, res){
     var prof = req.body.professor.split(',');
     var newCourse = {"courseTitle": req.body.courseTitle,
         "sectionNum": req.body.sectionNum,
-        "lastName": prof[0],
-        "firstName": prof[1],
-        "officeHour": {"location": req.body.office}};
-    collection.insert(newCourse, function (err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
+        "professor": {"lastName": prof[0]}, "firstName": prof[1], "officeHour": {"location": req.body.office}};
+    collection.insert(newCourse).then(function (docs){
+        console.log(docs)
+        if(docs){
+            res.render('addForm', {'message': "Course Added Successfully"});
+        }
     })
 
+}
+
+module.exports.searchCourse = function (req, res) {
+    var db = req.db;
+    var collection = db.get('course');
+    console.log(req.body);
+    collection.findOne({"courseTitle": req.body.courseTitle})
+        .then(function (docs) {
+            console.log(docs);
+            if(docs){
+                res.render('searchResult', {'data': docs});
+            }
+
+        })
 }
--- routes/coursesIndex.js
+++ routes/coursesIndex.js
@@ -9,9 +9,19 @@ router.get('/', ctrMain.get_courses);
 
 // get add course page
 router.get('/addCourse', function (req, res, next) {
-    res.render('addForm')
+    res.render('addForm', {'message': ""});
 })
 
+//add new course action
 router.post('/addCourse', ctrMain.add_course);
+
+//get search page
+router.get('/search', function (req, res, next) {
+    res.render('searchCourse');
+});
+
+//search db for the course
+router.post('/search', ctrMain.searchCourse);
+
 
 module.exports = router;
